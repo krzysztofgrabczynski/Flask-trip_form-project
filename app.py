80cb5c6d4bc373512862b746f5c533ab4e78e838
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-07-28 14:20:44 +0200

    Adding templates, implementing apperance of new_trip_form and menu by using bootstrap

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    else:
+        return render_template('trip_details.html')
+
+@app.route('/new_trip_form')
+def new_trip_form():
+    
+    return render_template('new_trip_form.html')
+
+if __name__ == '__main__':
+    app.run()
